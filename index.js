5ae58c53679cdee563699ccf144dd241fc7fd8c1
Henry Cooksley <henry.cooksley@gmail.com>
2019-01-16 11:40:33 +0000

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,42 @@ const b = tf.tensor([[1.0, 2.0, 3.0], [10.0, 20.0, 30.0]]);
 b.print();
 // Output: [[1 , 2 , 3 ],
 //          [10, 20, 30]]
+
+const c = tf.tensor2d([[1.0, 2.0, 3.0], [10.0, 20.0, 30.0]]);
+c.print();
+// Output: [[1 , 2 , 3 ],
+//          [10, 20, 30]]
+
+// 3x5 Tensor with all values set to 0
+const zeros = tf.zeros([3, 5]);
+zeros.print();
+// Output: [[0, 0, 0, 0, 0],
+//          [0, 0, 0, 0, 0],
+//          [0, 0, 0, 0, 0]]
+
+const initialValues = tf.zeros([5]);
+const biases = tf.variable(initialValues); // initialize biases
+biases.print(); // output: [0, 0, 0, 0, 0]
+
+const updatedValues = tf.tensor1d([0, 1, 0, 1, 0]);
+biases.assign(updatedValues); // update values of biases
+biases.print(); // output: [0, 1, 0, 1, 0]
+
+const d = tf.tensor2d([[1.0, 2.0], [3.0, 4.0]]);
+const d_squared = d.square();
+d_squared.print();
+// Output: [[1, 4 ],
+//          [9, 16]]
+
+const e = tf.tensor2d([[1.0, 2.0], [3.0, 4.0]]);
+const f = tf.tensor2d([[5.0, 6.0], [7.0, 8.0]]);
+
+const e_plus_f = e.add(f);
+e_plus_f.print();
+// Output: [[6 , 8 ],
+//          [10, 12]]
+
+const sq_sum = e.add(f).square();
+sq_sum.print();
+// Output: [[36 , 64 ],
+//          [100, 144]]
